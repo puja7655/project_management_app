6b4af8cd7fb1f9dbe89f18dfb21e1ace7f80d77c
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-03-04 14:00:00 +0100

    Added delete project functionality

--- src/App.jsx
+++ src/App.jsx
@@ -53,13 +53,26 @@ function App() {
     })
   }
 
+  function handleDeleteProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          project => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
   let selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
-  let content = <SelectedProject project={selectedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+
   if (projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>
   }
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar
